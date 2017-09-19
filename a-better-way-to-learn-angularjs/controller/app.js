d56fa593b4ac905e2641de5c4701c3852cf087c9
Diogo Simão <diogosimao@gmail.com>
2017-09-19 14:45:53 -0300

    Controller

---
+++ a-better-way-to-learn-angularjs/controller/app.js
@@ -0,0 +1,9 @@
+angular.module('app', []);
+
+angular.module('app').controller('MainCtrl', function ($scope) {
+  $scope.message = 'hello';
+  
+  $scope.updateMessage = function(message){
+    $scope.message = message;
+  }
+});
