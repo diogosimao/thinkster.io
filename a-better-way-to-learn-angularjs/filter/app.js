103890a7de11e135078777068d841661d746c7ec
Diogo Simão <diogosimao@gmail.com>
2017-09-20 09:10:40 -0300

    ngFilter

--- a-better-way-to-learn-angularjs/filter/app.js
+++ a-better-way-to-learn-angularjs/filter/app.js
@@ -9,7 +9,26 @@ function CapitalizerFilter() {
   }
 }
 
+function TestCtrl2() {
+  var self = this;
+  self.people = [
+    {
+      name: "Eric Simons",
+      born: "Chicago"
+    },
+    {
+      name: "Diogo Simao",
+      born: "Barra Mansa"
+    },
+    {
+      name: "Matthew Greenster",
+      born: "Virginia"
+    }
+  ];
+}
+
 angular.module('app', [])
 .controller('TestCtrl', TestCtrl)
+.controller('TestCtrl2', TestCtrl2)
 .filter('capitalize', CapitalizerFilter);
 
