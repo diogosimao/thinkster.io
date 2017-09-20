9717655c0e2fb357d20c7f3c91302d194cea3cb2
Diogo Simão <diogosimao@gmail.com>
2017-09-20 10:17:11 -0300

    Creating custom filters

--- a-better-way-to-learn-angularjs/filter/app.js
+++ a-better-way-to-learn-angularjs/filter/app.js
@@ -41,5 +41,19 @@ angular.module('app', [])
   ];
   this.filteredArray = filterFilter(this.array, 'a');
 }])
+.filter('reverse', function () {
+  return function(input, uppercase){
+    input = input || '';
+    var out = '';
+    for (var i = 0; i < input.length; i++) {
+      out = input.charAt(i) + out;
+    }
+    // conditional based on optional argument
+    if (uppercase){
+      out = out.toUpperCase();
+    }
+    return out;
+  }
+})
 .filter('capitalize', CapitalizerFilter);
 
