2679e8919aa2abdf55ee099e10935cab21cd2ec1
Diogo Simão <diogosimao@gmail.com>
2017-09-25 19:33:42 -0300

    Location

--- a-better-way-to-learn-angularjs/routing/resolve/app.js
+++ a-better-way-to-learn-angularjs/routing/resolve/app.js
@@ -6,18 +6,28 @@ app.config(function($routeProvider){
     controller: "AppCtrl",
     controllerAs: "app",
     resolve: {
-      app: function($q, $timeout) {
-        var defer = $q.defer();
-        $timeout(function () {
-          defer.resolve();
-        }, 2000);
-        return defer.promise;
-      }
+      loadData: appCtrl.loadData,
+      prepData: appCtrl.prepData
     }
   });
 })
-.controller('AppCtrl', function($scope) {
+var appCtrl = app.controller('AppCtrl', function($scope, $route) {
+  console.log($route);
   $scope.model = {
     message: "I'm a great app!"
   }
 });
+appCtrl.loadData = function($q, $timeout) {
+  var defer = $q.defer();
+  $timeout(function () {
+    defer.resolve("loadData");
+  }, 2000);
+  return defer.promise;
+}
+appCtrl.prepData = function($q, $timeout) {
+  var defer = $q.defer();
+  $timeout(function () {
+    defer.resolve("prepData");
+  }, 2000);
+  return defer.promise;
+}
