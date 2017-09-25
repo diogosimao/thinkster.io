b9fd1774ae5ac9c09469cc0d4b544c806a2fe8d8
Diogo Simão <diogosimao@gmail.com>
2017-09-25 17:19:35 -0300

    Routing params

--- a-better-way-to-learn-angularjs/routing/app.js
+++ a-better-way-to-learn-angularjs/routing/app.js
@@ -1,13 +1,19 @@
 var app = angular.module('app', [ 'ngRoute' ]);
 
 app.config(function($routeProvider){
-  $routeProvider.when("/", {
+  $routeProvider.when("/:firstName/:middleName/:lastName", {
     templateUrl: "app.html",
     controller: "AppCtrl",
     controllerAs: "app"
+  })
+  .when('/cookies', {
+    template: "NOM NOM NOM NOM"
+  })
+  .otherwise({
+    template: "This route isn't set!"
   });
 })
-.controller('AppCtrl', function() {
+.controller('AppCtrl', function($routeParams) {
   var self = this;
-  self.message = "The app routing is working!";
+  self.message = $routeParams.firstName + " " + $routeParams.middleName + " " + $routeParams.lastName;
 });
