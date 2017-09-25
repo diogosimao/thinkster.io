6fac6212370423fcdc63510f6fe7b0e86df2629b
Diogo Simão <diogosimao@gmail.com>
2017-09-25 16:13:53 -0300

    Remote and Inline Template

---
+++ a-better-way-to-learn-angularjs/templates/app.js
@@ -0,0 +1,16 @@
+(function () {
+  function TestCtrl ($templateCache){
+    this.user = {name: 'Blake'};
+    console.log($templateCache.get('test.html'));
+  }
+
+  angular.module('app', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      controller: 'TestCtrl as test',
+      templateUrl: 'test.html' 
+    })
+    .otherwise('/');
+  })
+  .controller('TestCtrl',TestCtrl);
+})()
