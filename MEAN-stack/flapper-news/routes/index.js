34c44f6c5b24b0cf343641e0b747a4c645c3273c
Diogo Simão <diogosimao@gmail.com>
2017-10-14 14:21:36 -0300

    Pre-loading Objects

--- MEAN-stack/flapper-news/routes/index.js
+++ MEAN-stack/flapper-news/routes/index.js
@@ -22,9 +22,24 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next( new Error('cant\'t find post'));}
+
+    req.post = post;
+    return next();
+  });
+});
+
 /*upvote a post - PUT /posts/:id/upvote - upvote a post, notice we use the post ID in the URL */
 
 /*view comments associated with a post - GET /posts/:id - return an individual post with associated comments */
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
 
 /*add a comment - POST /posts/:id/comments - add a new comment to a post by ID */
 
