d5988845fafdc7c63cbb3159f581fe9795bbdbb0
Diogo Simão <diogosimao@gmail.com>
2017-10-14 15:11:56 -0300

    actions a user can perform

--- MEAN-stack/flapper-news/routes/index.js
+++ MEAN-stack/flapper-news/routes/index.js
@@ -22,6 +22,7 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+//post middleware function
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -34,16 +35,62 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
+//comment middleware function
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment) {
+    if (err) { return next(err); }
+    if (!comment) { return next( new Error('cant\'t find comment'));}
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 /*upvote a post - PUT /posts/:id/upvote - upvote a post, notice we use the post ID in the URL */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
 
 /*view comments associated with a post - GET /posts/:id - return an individual post with associated comments */
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(req.post);
+  });
 });
 
 /*add a comment - POST /posts/:id/comments - add a new comment to a post by ID */
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if (err) {return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if (err) { return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
 
 /*upvote a comment - PUT /posts/:id/comments/:id/upvote - upvote a comment*/
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
