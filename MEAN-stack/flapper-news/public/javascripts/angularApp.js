349ef668a60360b05521a84725b4cdee05cdee15
Diogo Simão <diogosimao@gmail.com>
2017-10-14 16:03:43 -0300

    Wiring Everything Up (frontend + backend

--- MEAN-stack/flapper-news/public/javascripts/angularApp.js
+++ MEAN-stack/flapper-news/public/javascripts/angularApp.js
@@ -18,7 +18,12 @@ app.config([
     .state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
-      controller: 'PostsCtrl'
+      controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
     });
     $urlRouterProvider.otherwise('home');
 }]);
@@ -33,23 +38,56 @@ app.factory('posts', ['$http', function($http) {
     });
   };
 
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data) {
+        post.upvotes += 1;
+      });
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res) {
+      return res.data;
+    });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/post/' + id + '/comments', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+      .success(function(data) {
+        comment.upvotes += 1;
+      });
+  };
   return o;
 }]);
 app.controller('PostsCtrl', [
   '$scope',
-  '$stateParams',
   'posts',
-  function($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id];
+  'post',
+  function($scope, posts, post) {
+    $scope.post = post;
     $scope.addComment = function() {
       if($scope.body === '') { return; }
-      $scope.post.comments.push({
+      posts.addComment(post._id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
       });
       $scope.body = '';
-    }
+    };
+    $scope.incrementUpvotes = function(comment) {
+      posts.upvoteComment(post, comment);
+    };
 }]);
 app.controller('MainCtrl', [
   '$scope',
@@ -58,19 +96,14 @@ app.controller('MainCtrl', [
     $scope.posts = posts.posts;
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title, 
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
       });
       $scope.title = '';
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
 }]);
